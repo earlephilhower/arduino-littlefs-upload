a699fbd79e83efc551d9097e471de3c00737cc26
Earle F. Philhower, III <earlephilhower@yahoo.com>
2025-12-29 09:54:11 -0800

    Handle backslashes in image file path on Windows
    Fix path handling for OpenOCD on Windows.
    
    Fixes #117

--- src/extension.ts
+++ src/extension.ts
@@ -498,6 +498,9 @@ async function doOperation(context: vscode.ExtensionContext, arduinoContext: Ard
             if (arduinoContext.boardDetails.buildProperties['build.chip']) {
                 chip = arduinoContext.boardDetails.buildProperties['build.chip'];
             }
+            if (platform() === 'win32') {
+                imagefile = imagefile.replace(/\\/g, '/') // #117, OpenOCD on Windows seems to parse \s in name as escape chars
+            }
             uploadOpts = ["-f", "interface/cmsis-dap.cfg", "-f", "target/" + chip +".cfg", "-s", openocdPath + "/share/openocd/scripts",
                           "-c", "init; adapter speed 5000; program "+ imageFile + " verify 0x" + fsStart.toString(16) + "; reset; exit"];
         } else {
